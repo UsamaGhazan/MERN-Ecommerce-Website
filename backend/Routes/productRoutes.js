e41fc78225a744cb2e5dddf528223fad06614fc3
Usama <usamaghazan@yahoo.com>
2022-12-03 02:14:23 +0500

    Create and update Product endpoints added

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -4,11 +4,15 @@ import {
   getAllProducts,
   getSingleProduct,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../Controllers/ProductController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.get('/', getAllProducts);
+router.post('/', protect, admin, createProduct);
 router.delete('/:id', protect, admin, deleteProduct);
 router.get('/:id', getSingleProduct);
+router.put('/:id', protect, admin, updateProduct);
 
 export default router;
